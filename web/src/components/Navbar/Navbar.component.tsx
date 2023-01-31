0e1003f5d366ff68ab202b8c1504d0e2aef4578b
leandro <fahurleandro@gmail.com>
2023-01-30 19:52:25 -0800

    basic navbar with one link

--- web/src/components/Navbar/Navbar.component.tsx
+++ web/src/components/Navbar/Navbar.component.tsx
@@ -2,6 +2,29 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
+interface LinkProps {
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void; //React.Dispatch<React.SetStateAction<string>>
+}
+
+function Link(props: LinkProps) {
+  const { page, selectedPage, setSelectedPage } = props;
+  const lowerCasePage = page.toLowerCase();
+
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+}
+
 interface NavbarProps {
   selectedPage: string;
   setSelectedPage: (value: string) => void; //React.Dispatch<React.SetStateAction<string>>
@@ -13,5 +36,21 @@ export function Navbar(props: NavbarProps) {
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
 
-  return <nav></nav>;
+  return (
+    <nav className="z-40 w-full top-0 py-6">
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">JE</h4>
+
+        {isAboveMediumScreens && (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
